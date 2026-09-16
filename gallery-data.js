39b062b48850e79089c5733f9d2c7ce052bb78ad
Safoorah Sabir <sabirsafoorah@gmail.com>
2026-09-16 15:00:12 +0500

    Add gallery items for SPSS Hands-on Workshop
    Added multiple gallery items for the SPSS Hands-on Workshop.

--- gallery-data.js
+++ gallery-data.js
@@ -1,4 +1,37 @@
 window.galleryItems = [
-  // Add gallery items here. Example:
-  // { image: "gallery/idh-workshop-2026-01.jpg", title: "Biostatistics & SPSS Workshop", event: "IDH House Officers", date: "19 August 2026", alt: "Safoorah Sabir delivering a workshop" }
+  {
+    image: "gallery/SPSS.jpg",
+    title: "SPSS Hands-on Workshop",
+    event: "SRF Forum",
+    date: "10 September 2026",
+    alt: "SPSS hands on workshop"
+  },
+  {
+    image: "gallery/SPSS 1.jpg",
+    title: "SPSS Hands-on Workshop",
+    event: "SRF Forum",
+    date: "10 September 2026",
+    alt: "SPSS hands on workshop"
+  },
+  {
+    image: "gallery/SPSS 2.jpg",
+    title: "SPSS Hands-on Workshop",
+    event: "SRF Forum",
+    date: "10 September 2026",
+    alt: "SPSS hands on workshop"
+  },
+  {
+    image: "gallery/SPSS 3.jpg",
+    title: "SPSS Hands-on Workshop",
+    event: "SRF Forum",
+    date: "10 September 2026",
+    alt: "SPSS hands on workshop"
+  },
+  {
+    image: "gallery/SPSS 4.jpg",
+    title: "SPSS Hands-on Workshop",
+    event: "SRF Forum",
+    date: "10 September 2026",
+    alt: "SPSS hands on workshop"
+  }
 ];
