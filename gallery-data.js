d99c65c91370bb9d3f4a2b501025a1bc77236972
Safoorah Sabir <sabirsafoorah@gmail.com>
2026-09-17 08:38:42 +0500

    Change image extensions from .jpg to .jpeg
    Updated image file extensions from .jpg to .jpeg for gallery items.

--- gallery-data.js
+++ gallery-data.js
@@ -1,34 +1,34 @@
 window.galleryItems = [
   {
-    image: "gallery/SPSS.jpg",
+    image: "gallery/SPSS.jpeg",
     title: "SPSS Hands-on Workshop",
     event: "SRF Forum",
     date: "10 September 2026",
     alt: "SPSS hands on workshop"
   },
   {
-    image: "gallery/SPSS 1.jpg",
+    image: "gallery/SPSS 1.jpeg",
     title: "SPSS Hands-on Workshop",
     event: "SRF Forum",
     date: "10 September 2026",
     alt: "SPSS hands on workshop"
   },
   {
-    image: "gallery/SPSS 2.jpg",
+    image: "gallery/SPSS 2.jpeg",
     title: "SPSS Hands-on Workshop",
     event: "SRF Forum",
     date: "10 September 2026",
     alt: "SPSS hands on workshop"
   },
   {
-    image: "gallery/SPSS 3.jpg",
+    image: "gallery/SPSS 3.jpeg",
     title: "SPSS Hands-on Workshop",
     event: "SRF Forum",
     date: "10 September 2026",
     alt: "SPSS hands on workshop"
   },
   {
-    image: "gallery/SPSS 4.jpg",
+    image: "gallery/SPSS 4.jpeg",
     title: "SPSS Hands-on Workshop",
     event: "SRF Forum",
     date: "10 September 2026",
